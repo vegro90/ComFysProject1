eb9e5399c351bb5b2b12ce3461e38a4868b17567
vegro90 <tilrandom@gmail.com>
2016-09-19 16:49:58 +0200

    added file handeling in plotter
    Project1_plotter.py now plotts all figures in .eps and .png formats in
    respective subfolders

---
+++ Project1/Project1_plotter.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from pylab import*
+import scipy
+import numpy as np
+import os
+import re
+
+def removeFileExtension(fileName):
+    return re.sub(r"\.[a-zA-Z]+$", "", fileName)
+
+for i in range(1,4):
+    fileName = "Result_n=10^" + str(i) + ".txt" 
+    data = np.loadtxt(fileName, skiprows = 1)
+    x = data[:,0]
+    u = data[:,1]
+    v = data[:,2]
+    w = data[:,3]
+    error = data[:,4]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+
+    ax.plot(x,v,'r-', marker = '1' , label = r'Numerical solution, $v(x)$ ')
+    ax.plot(x,u,'b-', label = r'Analytical solution, $u(x)$ ')
+    xlabel(r'$x$', size=20, labelpad=5)
+    ylabel(r'$f(x)$', size=20, labelpad=5)
+    title(r'Aproximation for $n=$%i'%10**i  )
+    ax.grid()
+    ax.legend(loc='smart')
+    fileNameNoExtention = removeFileExtension(fileName)
+    fileDir = os.getcwd()
+    #pngDir = os.path.expanduser(fileDir + "/Images/png")
+    epsDir = os.path.join(fileDir,"Images", "eps")
+    pngDir = os.path.join(fileDir,"Images", "png")
+    if not os.path.exists(epsDir):
+        os.makedirs(os.path.join(fileDir, "Images", "eps"))
+    if not os.path.exists(pngDir):
+        os.makedirs(os.path.join(fileDir, "Images", "png"))
+    
+    fig.savefig(pngDir + "/" + fileNameNoExtention + ".png", bbox_inches='tight', format="png")
+    fig.savefig(epsDir + "/" +fileNameNoExtention + ".eps", bbox_inches='tight', format="eps")
+#show()
